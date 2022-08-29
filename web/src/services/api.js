33903a0eff4c74ea7a5995d73ea29144b3e41c7c
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-08-29 16:21:17 -0400

    alterando endereço do backend

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
 	//baseURL: 'http://192.168.0.120:3001/api/'
-	baseURL: 'http://192.168.100.3:3001/api/'
+	baseURL: 'http://192.168.10.146:3001/api'
 })
 
 export default api;
