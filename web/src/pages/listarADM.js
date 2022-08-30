8aaf31ed1a47fa4a5e7719ca5c76df3d08fe2ef6
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-08-30 16:36:28 -0400

    Update listarADM.js

--- web/src/pages/listarADM.js
+++ web/src/pages/listarADM.js
@@ -1,6 +1,7 @@
 
 import api from "../services/api";
 import React, {useEffect, useState} from 'react'
+import { useHistory } from "react-router-dom";
 import Sidebar from '../components/Sidebar'
 import '../styles/pages/listarUser.css';
 import {BsWrench} from 'react-icons/bs';
@@ -39,40 +40,78 @@ function AdministradoresMap(){
     const [list, setList] = useState([]);
 
     useEffect(()=>{
-        api.get("empresa/cadastradas")
+        const log = api.post(`/log/user/${localStorage.getItem('id_login')}`,{
+            acao:"Visualizou Todos os Registrados "
+          })
+    
+        api.get("adm/cadastrados")
       .then((todo)=> setList(todo.data));
     },[])
+    const history = useHistory()
+    const [busca, setBusca] = useState('');
+    async function handleSubmit() {
+
+
+        const response = await api.get(`/buscarADM?usuario=${busca}`)
+    
+        if(response.status == 200){
+          alert('Registrado encontrado')
+          history.push(`/adm/${response.data._id}`)
+        }else{
+          alert(response.statusText)
+        }
+     
+      }
     
         return (
             
             <div id="page-CreateUser">
-                  <Sidebar/>
-                  <div className="create-user-form" >
-                  <h2 style={text}>ADMINISTRADORES</h2>
+                <Sidebar/>
+                <div className="create-user-form" >
+                <div>
+                    <fieldset>
+                        <div className="input-block">
+                            <h2 style={text}>Busca</h2>
+                            <input id="busca" 
+                                value={busca} 
+                                onChange={event => setBusca(event.target.value)} />
+                        </div>
+                    </fieldset>
+                    <button onClick={handleSubmit}  type="submit" className= "botaoBuscar">
+                        Buscar
+                    </button>
+                </div>
+                  <h2 style={text}>REGISTRADOS</h2>
 
                   <ul>  
                 {list.map(adm => (
                         <div  style={listStyle}  >
-                        
-                           <span style={text}>{adm.name}</span>
-                           <Link to={`/adm/${adm._id}`} className="mostrar_dados">
-                                <button type="button" className= "botao">
-                                    <BsEye size={26} color="rgba(0, 0, 0, 0.6)"/>
-                                </button>
-                            </Link>
+                           
+                            <span style={text}>{adm.registro}</span>
+                            <span style={text}>{adm.name}</span>
+                            <span>
+                                <Link to={`/adm/${adm._id}`} className="mostrar_dados">
+                                    <button type="button" className= "botao">
+                                        <BsEye size={26} color="rgba(0, 0, 0, 0.6)"/>
+                                    </button>
+                                </Link>
+                                <Link to={`/admin/editadm/${adm._id}`} className="mostrar_dados">
+                                    <button type="button" className= "botao">
+                                        <BsWrench size={26} color="rgba(0, 0, 0, 0.6)"/>
+                                    </button>
+                                </Link>
                             
-                            <button type="button" className= "botao">
-                                <BsWrench size={26} color="rgba(0, 0, 0, 0.6)"/>
-                            </button>
-                            <Link to={`/doc/${adm._id}/${adm.name}`} className="mostrar_dados">
-                            <button type="button" className= "botao">
-                                <BiArchive size={26} color="rgba(0, 0, 0, 0.6)"/>
-                            </button>
-                            </Link>
-                            
-                            <button type="button" className= "botao">
-                                <BiPaperclip size={26} color="rgba(0, 0, 0, 0.6)"/>
-                            </button>
+                                <Link to={`/doc/${adm._id}/${adm.name}/${adm.registro}`} className="mostrar_dados">
+                                    <button type="button" className= "botao">
+                                        <BiArchive size={26} color="rgba(0, 0, 0, 0.6)"/>
+                                    </button>
+                                </Link>
+                                <Link to={`/upload/${adm._id}/${adm.registro}`} className="mostrar_dados">
+                                    <button type="button" className= "botao">
+                                        <BiPaperclip size={26} color="rgba(0, 0, 0, 0.6)"/>
+                                    </button>
+                                </Link>
+                            </span>
                         
                            
                        </div>
