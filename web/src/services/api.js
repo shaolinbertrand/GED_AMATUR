2069d4b2d30912a01df4384f83b372325dc032ae
JEAN BERTRAND PAIXAO DA SILVA <jean_bertrand93@hotmail.com>
2022-09-14 09:48:59 -0400

    alterações realizadas pela juliana

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -4,7 +4,8 @@ const api = axios.create({
 	//baseURL: 'http://192.168.0.120:3001/api/'
 	//baseURL: 'http://192.168.100.3:3001/api/'
 	//baseURL: 'http://192.168.10.146:3001/api' //ip cabeado
-	baseURL: 'http://27.78.101.104:3001/api' //ip wifi
+	//baseURL: 'http://27.78.101.104:3001/api' //ip wifi
+	baseURL: 'http://localhost:3001/api'
 })
 
 export default api;
